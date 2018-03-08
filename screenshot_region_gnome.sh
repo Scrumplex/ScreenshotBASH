37cfe0d4afeaa1fd6d4e3a2318bf5ff5492f5353
Sefa Eyeoglu <contact@scrumplex.net>
2018-03-08 17:21:18 +0100

    Combined scripts, drop sharenix and use curl only

--- screenshot_region_gnome.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIRECTORY=$(dirname "$0")
-FILENAME=$DIRECTORY/img/$(date +"%Y_%m_%d_%H-%M-%S")_$(pwgen -s 20 1).png
-gnome-screenshot -a -f $FILENAME
-$DIRECTORY/sharenix -o -c $FILENAME # -o: open link in browser -c: cipboard -n notifications
--- screenshot_region_kde.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIRECTORY=$(dirname "$0")
-FILENAME=$DIRECTORY/$(date +%s).png
-spectacle -r -b -n -o $FILENAME
-$DIRECTORY/sharenix -o -q $FILENAME
